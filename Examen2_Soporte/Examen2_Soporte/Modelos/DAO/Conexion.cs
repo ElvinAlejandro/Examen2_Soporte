4083e316c2acd110f3cd3e3697be73995795e369
Elvin Hernández <elvinh5@hotmail.com>
2021-11-13 19:27:43 -0600

    Cambio 1

---
+++ Examen2_Soporte/Examen2_Soporte/Modelos/DAO/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2_Soporte.Modelos.DAO
+{
+    //Almacena mi conexion de BD
+    public class Conexion
+    {
+        protected SqlConnection MiConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["SoporteConexion"].ConnectionString);
+    }
+}
